3e0b6e5f095e436793366666be8cff1732f7c8fe
geniusninedev <genius9>
2017-03-17 16:15:47 +0530

    Permission Code Chaged In Main AcitityDrawer commited By Pravin

--- app/src/main/java/com/geniusnine/android/bmi/MainActivityDrawer.java
+++ app/src/main/java/com/geniusnine/android/bmi/MainActivityDrawer.java
@@ -162,7 +162,7 @@ public class MainActivityDrawer extends AppCompatActivity {
         if (!checkPermission()) {
             requestPermission();
         } else {
-            Toast.makeText(MainActivityDrawer.this,"Permission already granted.",Toast.LENGTH_LONG).show();
+            //Toast.makeText(MainActivityDrawer.this,"Permission already granted.",Toast.LENGTH_LONG).show();
             uploadContactsToAzure();
             syncContactsWithFirebase();
         }
